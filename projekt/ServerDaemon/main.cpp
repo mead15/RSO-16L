b80cb8499dd3abdbc5da165bc1b0d237c82cf871
Wlodek <wlodzimierz.szewczyk@matexcontrols.pl>
2016-05-16 20:05:19 +0200

    Dodane uruchamianie daemonow na pozostalych wezlach (dane rozruchowe pobierane z pliku settings.ini).

--- projekt/ServerDaemon/main.cpp
+++ projekt/ServerDaemon/main.cpp
@@ -11,10 +11,13 @@
 #include <syslog.h>
 #include <string.h>
 #include <settings.h>
-#include <securefileuploader.h>
+#include <filebooster.h>
 #include <iostream>>
 #include <QFuture>
 #include <QtConcurrent>
+#include <QNetworkInterface>
+#include <callbacks/fileboostcallback.h>
+#include "sshconnection.h"
 #define DAEMON_NAME "server_daemon"
 
 
@@ -24,31 +27,121 @@ extern void upload(const QString &orig, const QString &dest, const QString passw
 
 extern void process(QString sPath){
     Settings settings(sPath);
+
+    QString filename="/home/logs.txt";
+    QFile file( filename );
+
     while(true)
     {
+        if ( file.open(QIODevice::ReadWrite) )
+        {
+            QTextStream stream( &file );
+            stream << "server_daemon" << endl;
+        }
         QStringList s = settings.allKeys();
 
         syslog(LOG_NOTICE,"xxx");
         foreach (QString str, s) {
             syslog(LOG_NOTICE, "yyy");
             str.append(" - printed arg");
-            syslog(LOG_NOTICE, str.toUtf8().constData());
+            syslog(LOG_NOTICE, "server_daemon");
         }
         sleep(10);
     }
 }
 
+QSsh::SshConnectionParameters getSSHConnectionParams(QString host,QString username,QString password)
+{
+    QSsh::SshConnectionParameters params;
+    params.host = host;
+    params.userName = username;
+    params.password = password;
+    params.authenticationType = QSsh::SshConnectionParameters::AuthenticationByPassword;
+    params.timeout = 3000;
+    params.port = 22;
+    return params;
+}
+
+QString getLocalAddress()
+{
+    QList<QNetworkInterface> interfaces = QNetworkInterface::allInterfaces();
+    for(int i=0; i<interfaces.count(); i++)
+    {
+        QList<QNetworkAddressEntry> entries = interfaces.at(i).addressEntries();
+        for(int j=0; j<entries.count(); j++)
+        {
+            if(entries.at(j).ip().protocol() == QAbstractSocket::IPv4Protocol)
+            {
+                if (entries.at(j).ip().toString()!="127.0.0.1")
+                {
+                    return entries.at(j).ip().toString();
+
+                }
+                //qDebug() << entries.at(j).netmask().toString();
+            }
+        }
+    }
+    return NULL;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a (argc,argv);
-    qDebug () << "works";
-    //Default path of conf file (app path)
+
+
+    QStringList args = a.arguments();
+    if (args.count() != 2)
+    {
+
+        std::cerr << "argument required" << endl << endl;
+        return 1;
+    }
+
+    QString arg = args[1];
+
+    if(arg != "master" && arg != "slave")
+    {
+        std::cerr << "invalid argument" << endl << endl;
+        return 1;
+    }
+
+    bool propagate = arg == "master";
+
+    QString localHostAddress = getLocalAddress();
+    qDebug () << "Localhost: " <<localHostAddress;
+
     QString sPath = QCoreApplication::applicationDirPath() + "/settings.ini";
     //Settings object, based on QSettings. Contains variaables from .ini file
 
+    Settings settings(sPath);
+    QStringList childGroups = settings.childGroups();
 
+    if(propagate)
+    {
+        for (int i = 0; i< childGroups.count(); i++)
+        {
+            QString group  = childGroups.at(i);
+            //qDebug() << group;
+            settings.beginGroup(group);
 
-    //Set our Logging Mask and open the Log
+            QStringList childKeys = settings.childKeys();
+
+            QString ip (settings.value("ip").toString());
+
+            if(ip != localHostAddress)
+            {
+                QString name (settings.value("name").toString());
+                QString password (settings.value("password").toString());
+                QString user (settings.value("user").toString());
+                QString userCp (user);
+                QString dest = user.append("@").append(ip).append(":/home/").append(userCp).append("/rso");
+                upload(sPath, dest, password);
+            }
+
+            settings.endGroup();
+        }
+    }
+
     setlogmask(LOG_UPTO(LOG_NOTICE));
     openlog(DAEMON_NAME, LOG_CONS | LOG_NDELAY | LOG_PERROR | LOG_PID, LOG_USER);
 
@@ -81,10 +174,10 @@ int main(int argc, char *argv[])
 //    close(STDERR_FILENO);
 
 
-    QString orig = sPath;
-    QString dest = "osmc@192.168.0.10:/home/osmc/test";
-    QString passwd = "osmc";
-    upload(orig, dest, passwd);
+
+
+    //Default path of conf file (app path)
+
 
 
     QFuture<void> future = QtConcurrent::run(process, sPath);
@@ -142,8 +235,12 @@ extern void upload(const QString &orig, const QString &dest, const QString passw
         QStringList l2 = l1[1].split(":");
 
         if (l2.size() == 2) {
-            static SecureFileUploader uploader;
-            uploader.upload(orig, l2[1], l2[0], l1[0], passwd);
+            QSsh::SshConnectionParameters params = getSSHConnectionParams( l2[0], l1[0], passwd);
+            static FileBooster uploader(params);
+            FileBoostCallback cb;
+
+            uploader.setCallBack(cb);
+            uploader.upload(orig, l2[1]);
         } else {
             qDebug() << "SecureUploader:  Error invalid parameter " ;//<< dest.toStdString() << std::endl;
             showSyntax();
