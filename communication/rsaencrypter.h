957537f48d38772db323895819f151b60db6779b
piotrek1234 <pqcinski+gh@gmail.com>
2016-05-23 11:01:01 +0200

    drobne poprawki (kody błędu)

--- communication/rsaencrypter.h
+++ communication/rsaencrypter.h
@@ -31,14 +31,3 @@ private:
 
 #endif // RSAENCRYPTER_H
 
-/*
- * QByteArray ma metody toBase64 i fromBase64
- *
- * encrypt przyjmuje QStringa z całą ramką do zaszyfrowania i QStringa z kluczem prywatnym odbiorcy,
- * powinien zwrócić QStringa z ramką zaszyfrowaną przez rsa i potem zakodowaną przez base64
- *
- * decrypt przyjmuje QStringa z wiadomością zaszyfrowaną przez rsa i zakodowaną przez base64,
- * trzeba odkodować base64 a potem odszyfrować rsa własnym kluczem prywatnym i zwrócić QStringa z odszyfrowaną ramką
- *
- * setPrivateKey - wiadomo
- */
